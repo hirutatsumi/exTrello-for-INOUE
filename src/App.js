e5c67b45211039fc8353a214e83ad50efce600fc
hirutatsumi <tatsumi_hiruta@ex-ex.co.jp>
2023-04-06 14:51:32 +0900

    ver.demo done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./components/header/Header";
+import { TaskCards } from "./components/header/task/TaskCards";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <Header />
+      <TaskCards />
     </div>
   );
 }
 
 export default App;
+
+// 井上様からの要望
+//カード内の背景色を変更する
+// STEP1: 右クリックで"color"をコンソールログで出力させる
+
+//カードをクリックしたら備考を記載できるモーダルを表示
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>eX Trello for INOUE</h1>
+      </header>
+    </div>
+  );
+};
